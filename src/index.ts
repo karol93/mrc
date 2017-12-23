c6863d825a90bef5ad3c524a546365985c1809cf
Karol Suchecki <kajoj7@gmail.com>
2017-12-23 19:02:40 +0100

    alert

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,21 @@ function saveFilters(e: any) {
     filters.hideFromOutside = $("#hide-from-outside").prop('checked');
     filters.priceTo = Number($("#price-to").val());
     filters.priceFrom = Number($("#price-from").val());
-    chrome.storage.sync.set(filters, () => { alert('saved!') });
+    chrome.storage.sync.set(filters, () => {
+        $(".alert").fadeToggle("slow", "linear")
+        setTimeout(() => {
+            $(".alert").fadeToggle("slow", "linear")
+        }, 2000);
+    });
 }
 
 function loadFilters() {
     chrome.storage.sync.get(new Filters(), (filters: Filters) => {
 
-        $("#active-filtring").prop('checked',filters.activeFiltring);
+        $("#active-filtring").prop('checked', filters.activeFiltring);
         $("#hide-not-matched").prop('checked', filters.hideNotMatched);
         $("#hide-from-outside").prop('checked', filters.hideFromOutside);
     });
 }
+
+$(".times").click(() => $(".alert").fadeToggle("slow", "linear"))
