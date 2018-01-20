410d735afcb93875dcbe98a0460243033161be37
Karol Suchecki <kajoj7@gmail.com>
2018-01-20 15:55:36 +0100

    webpack common chunk

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,20 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        vendor: ['jquery', 'babel-polyfill'],
-        utils: path.join(__dirname, 'src/utils.ts'),
-        filters: path.join(__dirname, 'src/filters.ts'),
         index: path.join(__dirname, 'src/index.ts'),
         background: path.join(__dirname, 'src/background.ts'),
         content_script: path.join(__dirname, 'src/content_script.ts'),
+        vendor: ['jquery', 'babel-polyfill'],    
     },
     output: {
         path: path.join(__dirname, 'dist/js'),
-        filename: '[name].js'
+        filename: "[name].js",
     },
     module: {
         loaders: [{
             exclude: /node_modules/,
             test: /\.tsx?$/,
-            loader: 'babel-loader?presets[]=es2015!ts-loader'
+            loader: 'babel-loader?presets[]=env!ts-loader'
         }]
     },
     resolve: {
@@ -26,12 +24,11 @@ module.exports = {
     },
     plugins: [
 
-        // pack common vender files
         new webpack.optimize.CommonsChunkPlugin({
-            names: ['vendor', 'utils','filters'],
-            minChunks: Infinity
-        })
-      
+            names: ['common','vendor'],
+            minChunks: 2
+        }),
+
 
         // minify
         // new webpack.optimize.UglifyJsPlugin()
