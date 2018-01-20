00637ac590580942c35994b265a44fbf0b20d51c
Karol Suchecki <kajoj7@gmail.com>
2018-01-20 17:24:42 +0100

    depend on concrete endpoints not host only

--- src/chrome-extension/background.ts
+++ src/chrome-extension/background.ts
@@ -1,20 +1,14 @@
 import { Filters } from './../shared/filters';
 import { AvailableEndpoints } from './../config/config';
-import { Utils } from "../shared/utils";
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && tab.active) {
-        var hostname = Utils.getHostnameFromUrl(tab.url);
-        if (AvailableEndpoints.indexOf(hostname) > -1) {
-            chrome.storage.sync.get(new Filters(), (filters: Filters) => {
-                if (filters.activeFiltring) {
-                    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                        chrome.tabs.sendMessage(tabs[0].id, { filters: filters, hostname: hostname }, (response) => {
-                            console.log(response);
-                        });
-                    });
-                }
-            });
-        }
+    if (changeInfo.status == 'complete' && tab.active && AvailableEndpoints.filter(p => tab.url.indexOf(p) > -1).length) {
+        chrome.storage.sync.get(new Filters(), (filters: Filters) => {
+            if (filters.activeFiltring) {
+                chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                    chrome.tabs.sendMessage(tabs[0].id, { filters: filters, url: tab.url })
+                });
+            }
+        });
     }
 })
--- src/chrome-extension/content_script.ts
+++ src/chrome-extension/content_script.ts
@@ -1,20 +1,18 @@
 import { Filters } from './../shared/filters';
+import { Utils } from './../shared/utils';
 import { ScraperFactory } from './../scraper/scraper-factory';
 import { IScraper } from '../scraper/scraper';
 
 import 'babel-polyfill';
 
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  
-    let hostname : string = msg.hostname;
+chrome.runtime.onMessage.addListener((msg, sender) => {
+    let hostname : string = Utils.getHostnameFromUrl(msg.url);
     let filters : Filters = msg.filters;
 
     let scraper: IScraper = ScraperFactory.createScraper(hostname);
 
     scraper.scrap(filters);
-
-    sendResponse(1);
 });
 
 
