99f9ea17a275989748e5762663e54b42827add2f
Karol Suchecki <kajoj7@gmail.com>
2018-01-21 13:37:59 +0100

    otodom scraper - highlight

--- src/scraper/otodom-scraper.ts
+++ src/scraper/otodom-scraper.ts
@@ -24,14 +24,16 @@ export class OtodomScraper implements IScraper {
                     const data = await $.get(detailsUrl);
                     const additionalPrice = $(data).find("strong:contains('Czynsz - dodatkowo:')").parent().contents().eq(1).text().replace('zł', '').replace(' ', '');
                     if (!$.isNumeric(additionalPrice)) {
-                        priceCell.append('<div>brak informacji o czynszu</div>');
+                        priceCell.css({ 'color': '#bebebe', 'font-size': '12px', "text-align": "right" })
+                        priceCell.append('<div style="font-size: 18px;color: #ff7200;">brak informacji o czynszu</div>');
                         continue; 
                     } 
 
                     const originalPrice = $(data).find('ul.main-list li:eq(0)').find('span').text().replace('zł', '').replace(' ', '');
                     const sumPrice = Number(originalPrice) + Number(additionalPrice);
                     if (sumPrice > filters.priceFrom && sumPrice < filters.priceTo) {
-                        priceCell.html(sumPrice.toString());
+                        row.parent().css({'border':'4px solid #13ba58','background':'#13ba580d'})
+                        priceCell.html(`<div style="text-align: right"><div style="">Suma: ${sumPrice.toString()} zł</div><div style="margin-top: 4px;color: #13ba58;font-size: 13px;">Czynsz: ${additionalPrice} zł</div><div style="font-size: 13px;">Oryginalna cena: ${originalPrice} zł</div></div>`);
                     }
                     else if (filters.hideNotMatched) {
                         row.parent().remove();
@@ -45,7 +47,7 @@ export class OtodomScraper implements IScraper {
                 row.parent().remove();
             }
             else {
-                priceCell.append('<div><strong style="color:#af3c3c;">OGŁOSZENIA SPOZA OTODOM</strong></div>');
+                priceCell.append('<div ><strong style="color:#af3c3c;">OGŁOSZENIA SPOZA OTODOM</strong></div>');
             }
         }
     }
